fb64f8c5b0f2eeecbc49548b29b3cde50f107620
konradzajda <konrad.zajda.c@icloud.com>
2023-01-11 12:18:12 +0100

    Add not found scenario to query handler

--- src/Application/Handlers/Queries/GetBudgetByIdQueryHandler.cs
+++ src/Application/Handlers/Queries/GetBudgetByIdQueryHandler.cs
@@ -1,15 +1,32 @@
-﻿using System.Threading;
+﻿using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 using Tivix.BudgetPlanner.Application.Abstractions;
 using Tivix.BudgetPlanner.Application.Queries;
+using Tivix.BudgetPlanner.Infrastructure;
 
 namespace Tivix.BudgetPlanner.Application.Handlers.Queries;
 
 public class GetBudgetByIdQueryHandler : IRequestHandler<GetBudgetByIdQuery, IApplicationResponse<BudgetViewModel>>
 {
+    private readonly IBudgetsContext _context;
+
+    public GetBudgetByIdQueryHandler(IBudgetsContext context)
+    {
+        _context = context;
+    }
+    
     public Task<IApplicationResponse<BudgetViewModel>> Handle(GetBudgetByIdQuery request, CancellationToken cancellationToken)
     {
-        return Task.FromResult(ApplicationResponse.NotFound<BudgetViewModel>());
+        var budget = _context.Budgets
+            .AsNoTracking()
+            .SingleOrDefault(y => y.Id == request.Id);
+
+        if (budget == null)
+            return Task.FromResult(ApplicationResponse.NotFound<BudgetViewModel>());
+
+        return Task.FromResult(ApplicationResponse.Success(new BudgetViewModel()));
     }
 }
---
+++ tests/Application.Tests/Handlers/Queries/GetBudgetByIdQueryHandlerTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NSubstitute;
+using Tivix.BudgetPlanner.Application.Abstractions;
+using Tivix.BudgetPlanner.Application.Queries;
+using Tivix.BudgetPlanner.Infrastructure.Entities;
+using Xunit;
+
+namespace Tivix.BudgetPlanner.Application.Handlers.Queries;
+
+public class GetBudgetByIdQueryHandlerTests
+{
+    private readonly GetBudgetByIdQueryHandler _sut;
+    private readonly IBudgetsContext _context;
+
+    public GetBudgetByIdQueryHandlerTests()
+    {
+        _context = Substitute.For<IBudgetsContext>();
+        _sut = new GetBudgetByIdQueryHandler(_context);
+    }
+    
+    [Fact]
+    public async Task Handle_ShouldReturnNotFoundResponse_WhenBudgetNotFound()
+    {
+        // Given
+        var query = new GetBudgetByIdQuery
+        {
+            Id = Guid.NewGuid(),
+        };
+        
+        _context.Budgets.Returns(Array.Empty<BudgetEntity>().AsQueryable());
+        
+        // When
+        var result = await _sut.Handle(query, CancellationToken.None);
+        
+        // Then
+        result.StatusCode.Should().Be(HttpStatusCode.NotFound);
+    }
+}
