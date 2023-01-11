8ece1c00c1b0a6d7e32ddfe040f2f70e4e3ee4d6
konradzajda <konrad.zajda.c@icloud.com>
2023-01-11 02:05:46 +0100

    Budgets Controller added with HTTP GET (by id) endpoint
    - MediatR added to decouple API and application layers
    - Added some simply controller's method tests to verify status codes returned

---
+++ src/Application/Abstractions/IApplicationResponse.cs
@@ -0,0 +1,14 @@
+﻿using System.Net;
+
+namespace Tivix.BudgetPlanner.Application.Abstractions;
+
+public interface IApplicationResponse<out T>
+{
+    T? Resource { get; }
+    
+    string[]? Errors { get; }
+    
+    HttpStatusCode StatusCode { get; }
+
+    bool Success => (Errors == null || Errors.Length == 0);
+}
---
+++ src/Application/Queries/GetBudgetByIdQuery.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using MediatR;
+using Tivix.BudgetPlanner.Application.Abstractions;
+
+namespace Tivix.BudgetPlanner.Application.Queries;
+
+public class GetBudgetByIdQuery : IRequest<IApplicationResponse<BudgetViewModel>>
+{
+    public Guid Id { get; init; }
+}
+
+public class BudgetViewModel
+{
+}
